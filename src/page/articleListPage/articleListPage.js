8eafb4bf185b71b93f516b3f419869f7a4a2c563
hokeys <substantiation@naver.com>
2019-06-29 00:14:19 +0900

    에디터 라우팅 연결 해야함

--- src/page/articleListPage/articleListPage.js
+++ src/page/articleListPage/articleListPage.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import Notice from '../../component/common/header'
 import ArticleListHeader from '../../component/common/articleListHeader'
+import {Switch , Route } from 'react-router-dom'
+import Editor from '../../component/common/textEditor'
 
 export default class ArticleListPage extends Component {
     render() {
         return (
             <div className="articleListPage">
                 <Notice></Notice>
-                <ArticleListHeader></ArticleListHeader>
+                <Switch>
+                     <Route path="/agora" exact component={ArticleListHeader} />
+                     <Route path="/agora/addArticle" component={Editor}/>
+                </Switch>
             </div>
         )
     }
